e528b6bf170b7f101d80edae89b6415862a26483
tadevi <1611695@hcmut.edu.vn>
2019-04-21 01:16:00 +0700

    Update code...

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,22 +1,22 @@
-BUILD_PATH=../build
-SOURCE_PATH=../src/
+BUILD_PATH=build
+SOURCE_PATH=src
 PACKAGE_NAME=rdi
 JAR_PATH=jar
 JAR_NAME=RemoveDuplicateImages.jar
 
-echo "Ensure 'hadoop' command in PATH"
-echo "1. Compiling sources............."
+echo -e "\tEnsure 'hadoop' command in PATH"
+echo -e "\t1. Compiling sources............."
 javac -cp `hadoop classpath` -d $BUILD_PATH $SOURCE_PATH/*.java
 if [ $? -ne 0 ]; then
 	exit;
 fi;
-echo "2. Creating Manifest............."
+echo -e "\t2. Creating Manifest............."
 echo "Main-Class: rdi.RDIDriver" > $BUILD_PATH/$PACKAGE_NAME/Manifest.txt
-echo "3. Createing jar archive........."
+echo -e "\t3. Createing jar archive........."
 cd $BUILD_PATH
 mkdir -p $JAR_PATH
 jar cfm $JAR_NAME $PACKAGE_NAME/Manifest.txt $PACKAGE_NAME/*.class
 mv $JAR_NAME $JAR_PATH
-echo "Finished"
+echo -e "\tFinished"
 
 
--- scripts/preprocessing.sh
+++ scripts/preprocessing.sh
@@ -1,26 +1,26 @@
-BUILD_PATH=../build
-SOURCE_PATH=../src/preprocessing
+BUILD_PATH=build
+SOURCE_PATH=src/preprocessing
 PACKAGE_NAME=rdipreprocessing
 JAR_PATH=jar
 JAR_NAME=RDIPreprocessing.jar
-echo "\tEnsure 'hadoop' command in PATH"
+echo -e "\tEnsure 'hadoop' command in PATH"
 
-echo "\t1. Compiling sources............."
+echo -e "\t1. Compiling sources............."
 
 	javac -cp `hadoop classpath` -d $BUILD_PATH $SOURCE_PATH/*.java
 
 	if [ $? -ne 0 ]; then
 		exit;
 	fi;
-echo "\t2. Creating Manifest............."
+echo -e "\t2. Creating Manifest............."
 
 	echo "Main-Class: rdipreprocessing.ImagesToSequence" > $BUILD_PATH/$PACKAGE_NAME/Manifest.txt
 
-echo "\t3. Createing jar archive........."
+echo -e "\t3. Createing jar archive........."
 	cd $BUILD_PATH
 	mkdir -p $JAR_PATH
 	jar cfm $JAR_NAME $PACKAGE_NAME/Manifest.txt $PACKAGE_NAME/*.class
 	mv $JAR_NAME $JAR_PATH
 
-echo "Finished"
+echo -e "Finished"
 
